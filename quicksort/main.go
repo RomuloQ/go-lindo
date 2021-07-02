a9d1bd641ba05ea98a6ad360db2eeb7433eb2c3e
Romulo Queiroz <romuloqueiroz100@gmail.com>
2021-07-01 23:47:17 -0300

    quicksort

---
+++ quicksort/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(quickSort([]int{5, 6, 7, 2, 1, 0}, 0, 5))
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func partition(arr []int, low int, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
